6bb428d4408524bda9b33179acef96255d8d3072
Han-Lon <josephmaxmorris7@gmail.com>
2024-08-02 00:21:56 -0500

    make it harder to download images by hiding context menu from image files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 import "./menu.js";
 import "./gallery.js";
 import "./lightbox.js";
+
+document.addEventListener('contextmenu', function(event) {
+    if (event.target.tagName.toLowerCase() === 'img') {
+        event.preventDefault();
+    }
+});
