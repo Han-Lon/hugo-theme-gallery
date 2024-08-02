bba0d31603ad2fa2e6f2f55867b2177b563d610e
Han-Lon <josephmaxmorris7@gmail.com>
2024-08-02 00:37:13 -0500

    add context menu handling for mobile (long press v short press)

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,32 @@ import "./menu.js";
 import "./gallery.js";
 import "./lightbox.js";
 
+// For mobile touch handling
+let touchStartTime = 0;
+const LONG_PRESS_THRESHOLD = 500; // 500ms for a long press
+
 document.addEventListener('contextmenu', function(event) {
     if (event.target.tagName.toLowerCase() === 'img') {
         event.preventDefault();
+    }
+});
+
+// Prevent bringing up context menu on mobile devices with a long press
+document.addEventListener('touchstart', function(event) {
+    if (event.target.tagName.toLowerCase() === 'img') {
+        touchStartTime = Date.now();
+    }
+});
+
+document.addEventListener('touchend', function(event) {
+    if (event.target.tagName.toLowerCase() === 'img') {
+        const touchEndTime = Date.now();
+        const touchDuration = touchEndTime - touchStartTime;
+
+        if (touchDuration >= LONG_PRESS_THRESHOLD) {
+            // It's a long press, prevent default behavior
+            event.preventDefault();
+        }
+        // If it's a quick tap, don't prevent default behavior
     }
 });
